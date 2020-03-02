8f2764c9e8d834edc40a73178f790af9b33e09ba
Andrea Petriku <iam.andreapetriku@gmail.com>
2020-03-02 16:34:34 +0100

    added temporary json file for stroing ratings

--- recipes/views.py
+++ recipes/views.py
@@ -6,6 +6,7 @@ import requests
 from ipware import get_client_ip
 from django.http import HttpResponse
 from .forms import UserForm
+import json
 
 # Create your views here.
 
@@ -28,7 +29,7 @@ def recipe_id_view(request, id):
     print(currentRecipe.mealName)
 
     # store id number to temporary file
-    with open("recipes/data/temp.json", "w") as out:
+    with open("recipes/data/temp_id.json", "w") as out:
         data = str(id)
         out.write(data)
 
@@ -36,18 +37,18 @@ def recipe_id_view(request, id):
 
 # success view
 def success_view(request):
-    # get id from the file stored in recipe_id_view
-    with open("recipes/data/temp.json", "r") as file:
+    # get id from json file stored in recipe_id_view
+    with open("recipes/data/temp_id.json", "r") as file:
         data_list = [id for id in next(file)]
         id = data_list[0]
 
+    # crop the IP address
     ip_tuple = get_client_ip(request)
 
     ip = str(ip_tuple)
     removed_chars = ["'", "(", ")", ",", "False", " "]
     for char in removed_chars:
         ip = ip.replace(char, "")
-
     lastIP = ip
 
     # check of POST request happened and process it
@@ -57,17 +58,30 @@ def success_view(request):
         # is_valid() needs to have required=True in every form item
         if form.is_valid():
             selected = form.cleaned_data.get("radio_button")
-
             selectedInt = int(selected)
 
             # write current meal ID and IP address to the database
             Recipe.objects.filter(id=id).update(mealDbRating=selectedInt) # rating
             Recipe.objects.filter(id=id).update(mealIP=lastIP) # last visitor ip
 
-            # todo: 2. calculate average rating for every meal and display it on the success page
+            # create a json file and append collected data to it
+            data_dict = {}
+            data_dict[id]=selectedInt
+            print(data_dict)
+
+            # data_list = []
+            # data_list.append(data_dict)
+            # print(data_list)
+
+            with open("recipes/data/data.json", "a+") as file:
+                json.dump(data_dict, file, indent=4, separators=(',', ': '))
+                print(file)
+
+            # todo: calculate average rating for every meal and display it on the success page
 
             return render(request, 'success.html', {'selected': selected,
                                                     'lastIP': lastIP,
+                                                    'id': id,
                                                    })
     else:
         return HttpResponse('An error occured!')
