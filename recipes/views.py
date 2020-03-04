36f8766a3fd081848d166c73b1d582b2f5103602
Andrea Petriku <iam.andreapetriku@gmail.com>
2020-03-04 14:50:33 +0100

    still not finished average results calculation

--- recipes/views.py
+++ recipes/views.py
@@ -67,29 +67,63 @@ def success_view(request):
             # create a json file and append collected data to it
             data_dict = {}
             data_dict[id]=selectedInt
-            print(data_dict)
 
-            # data_list = []
-            # data_list.append(data_dict)
-            # print(data_list)
-
+            # read the old json file, store it to list
             with open("recipes/data/data.json", "r") as file_read:
                 data = json.load(file_read)
-                print('data=', data)
-                print(type(data))
 
+                # convert id number to string needed in following dict
                 str_id = str(id)
+                # make a dict with current single record
                 single_dict_record = {str_id:selectedInt}
 
+                # add new collected data to the list
                 data.append(single_dict_record)
                 print("dict with item added:", data)
 
-
+            # write new list with data to json file and beautize it
             with open("recipes/data/data.json", "w") as file_write:
                 json.dump(data, file_write, indent=4, separators=(',', ': '))
 
             # todo: calculate average rating for every meal and display it on the success page
-            
+            print("data=",data)
+
+            # count all keys
+            for key in data:
+                print(key.keys())
+
+            # printing initial_dictionary
+            print("initial_dictionary", str(data))
+
+            # finding duplicate values
+            # from dictionary using flip
+            flipped_dict = {}
+
+            for item in data:
+                for key, value in item.items():
+                    if key not in flipped_dict:
+                        flipped_dict[key] = [value]
+                    else:
+                        flipped_dict[key].append(value)
+
+            # printing result
+            print("final_dictionary", str(flipped_dict))
+
+            # for each flipped_dict item calculate the average of values
+            for key, value in flipped_dict.items():
+                print('\nkey', key)
+                for vals in flipped_dict.values():
+                    print('vals', vals)
+                    suma = sum(vals)
+                    print('suma', suma)
+                    cnt = len(vals)
+                    print('cnt', cnt)
+                    avg = suma // cnt
+                    print('avg', avg)
+
+
+            # store results to new lists or dicts
+            # display values according to keys and values
 
             return render(request, 'success.html', {'selected': selected,
                                                     'lastIP': lastIP,
