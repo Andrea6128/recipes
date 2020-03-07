4fd368bee2831cf4bc192b233700b321b24ded36
Andrea Petriku <iam.andreapetriku@gmail.com>
2020-03-07 20:09:28 +0100

    working charts, added favicon

--- myproject/settings.py
+++ myproject/settings.py
@@ -79,8 +79,8 @@ DATABASES = {
     'default': {
     'ENGINE': 'django.db.backends.postgresql_psycopg2',
     'NAME': 'recipes',
-	'USER': 'andrea',
-	'PASSWORD': 'hgtr',
+	'USER': '',
+	'PASSWORD': '',
 	'HOST': '127.0.0.1',
 	'PORT': '5432',
     }
