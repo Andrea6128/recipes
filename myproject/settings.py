26feb611f540c02075c3f335626a2873f5fda16e
Andrea Petriku <iam.andreapetriku@gmail.com>
2020-03-10 12:52:41 +0100

    another css changes

--- myproject/settings.py
+++ myproject/settings.py
@@ -79,8 +79,8 @@ DATABASES = {
     'default': {
     'ENGINE': 'django.db.backends.postgresql_psycopg2',
     'NAME': 'recipes',
-	'USER': '',
-	'PASSWORD': '',
+	'USER': 'postgres',
+	'PASSWORD': 'postgres',
 	'HOST': '127.0.0.1',
 	'PORT': '5432',
     }
